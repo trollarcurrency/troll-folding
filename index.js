2400d486f2452f467f5350b06c6c4560cbb2f269
trollarcurrency <arebtcfeesconstant@gmail.com>
2021-07-10 03:25:26 -0700

    more work on web scraping + database saving

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const start = new Date().getTime();
+
 const fetch = require('undici-fetch');
-const fs = require('fs');
+const fs = require('fs').promises;
+const util = require('util');
+const csv_stringify = util.promisify(require('csv-stringify'));
+const sqlite3 = require('sqlite3');
+const open = require('sqlite').open;
+
 const fah_url = "https://apps.foldingathome.org/teamstats/team234980.html";
 
-async function main() {
+async function get_api_data() {
 	const response = await fetch(fah_url);
 	var body = await response.text();
 	console.log("Fetched",time());
@@ -16,14 +22,38 @@ async function main() {
 	for (var i = 5; i < nodes.length; i+= 10) {
 		result[
 			nodes[i]
-		] = nodes[i+2];
+		] = parseInt(nodes[i+2]);
 	}
 	console.log("Processed",time());
 	
-	fs.writeFile('result.txt', JSON.stringify(result), function(err) {
-		if (err) console.log(err);
-		console.log("Finished",time());
-	});
+	return result;
+}
+
+async function main() {
+	const current_data = await get_api_data();
+	var previous_data;
+	try {
+		previous_data = JSON.parse(await fs.readFile("./data/previous_scraped.json"));
+		if (Object.keys(current_data).length < Object.keys(previous_data).length) throw new Error();
+	} catch {
+		console.error("Prev data inacc./corr.");
+		process.exit();
+	}
+	
+	const work_json = [];
+	for (var key in current_data) {
+		if (previous_data[key] == current_data[key]) continue;
+		const credit = current_data[key] - (previous_data[key] || 0);
+		work_json.push({
+			"id": key,
+			"credit": credit,
+			"date": start
+		});
+	}
+	
+	const work_csv = await csv_stringify(work_json);
+	await fs.writeFile('./temp_work.csv', work_csv);
+	console.log("Written",time());
 }
 
 function time() {
